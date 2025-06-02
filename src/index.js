31258908590e1a9df0eb836685cd8b51ae9b7f44
ken168168ken <allfine.awml@gmail.com>
2025-06-02 17:04:31 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';    // 一定要把 Tailwind 的 CSS import 進來
-import App from './App';
+import './index.css';                      // 引入 Tailwind 及自定義 CSS
+import QueryMainPage from './components/QueryMainPage';
 
+// React 18+ 的寫法
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* 這裡先給 username="KenLee"，之後可接登入系統動態替換 */}
+    <QueryMainPage username="KenLee" />
   </React.StrictMode>
 );
